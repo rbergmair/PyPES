53b01862f30f9af5e2d74e2886bfe82da2af4944
Richard Bergmair <r.bergmair.gh@rb.uref.email>
2007-11-15 14:12:15 +0000

    git-svn-id: file:///dta/svn/old/PyPES/trunk@49 91a45504-6d28-49e1-87dd-05d800df9125

--- src/pyrmrs/mrs/robust/rmrsem.py
+++ src/pyrmrs/mrs/robust/rmrsem.py
@@ -326,12 +326,11 @@ class RMRSem( pyrmrs.mrs.common.mrsem.MRSem ):
     
     roots.append( curgrp );
     del roots[ 0 ];
-    
     print roots;
     
     fragments = {};
-    cons = {};
     root_by_lid = {};
+    quant_labels = [];
     
     for k in range( 0, len(roots) ):
       root = roots[ k ];
@@ -339,29 +338,73 @@ class RMRSem( pyrmrs.mrs.common.mrsem.MRSem ):
       for lid in root:
         root_by_lid[ lid ] = k;
         if self.rargs_by_lid.has_key( lid ):
-          if self.rargs_by_lid.has_key( lid ):
-            rstr = None;
-            body = None;
-            if self.rargs_by_lid[ lid ].has_key( "RSTR" ):
-              rstr = self.rargs_by_lid[ lid ][ "RSTR" ].var.vid;
-            if self.rargs_by_lid[ lid ].has_key( "BODY" ):
-              body = self.rargs_by_lid[ lid ][ "BODY" ].var.vid;
-            if ( not ( body is None ) ) and ( not ( rstr is None ) ):
-              holes.append( rstr );
-              holes.append( body );
-      fragments[ k ] = holes;
+          rstr = None;
+          body = None;
+          if self.rargs_by_lid[ lid ].has_key( "RSTR" ):
+            rstr = self.rargs_by_lid[ lid ][ "RSTR" ].var.vid;
+          if self.rargs_by_lid[ lid ].has_key( "BODY" ):
+            body = self.rargs_by_lid[ lid ][ "BODY" ].var.vid;
+          if ( not ( body is None ) ) and ( not ( rstr is None ) ):
+            holes.append( (False,rstr) );
+            holes.append( (False,body) );
+            quant_labels.append( lid );
+      fragments[ (True,k) ] = holes;
+
+    bindings = {};
+    
+    for ep in self.eps:
+      if ep.label.vid in quant_labels:
+        bindings[ ep.var.referent ] = ep.label.vid;
+
+    cons = {};
+    
+    for ep in self.eps:
+      if not ep.label.vid in quant_labels:
+        refs = [ ep.var.referent ];
+        if self.rargs_by_lid.has_key( ep.label.vid ):
+          args = self.rargs_by_lid[ ep.label.vid ];
+          for arg in args:
+            assert not args[ arg ].var is None;
+            assert bindings.has_key( args[ arg ].var.referent );
+            binding = bindings[ args[ arg ].var.referent ];
+            assert root_by_lid.has_key( binding );
+            upper = ( True, root_by_lid[binding] );
+            lower = ( True, root_by_lid[ep.label.vid] );
+            if not cons.has_key( upper ):
+              cons[ upper ] = [];
+            cons[ upper ].append( lower )
     
     for hcon in self.hcons:
       assert hcon.lovar is None;
-      hole = hcon.hi.vid;
-      root = root_by_lid[ hcon.lolbl.vid ];
-      if not cons.has_key( hole ):
-        cons[ hole ] = [];
-      cons[ hole ].append( root );
+      upper = ( False, hcon.hi.vid );
+      lower = ( True, root_by_lid[ hcon.lolbl.vid ] );
+      if not cons.has_key( upper ):
+        cons[ upper ] = [];
+      cons[ upper ].append( lower );
     
     solver = pyrmrs.tools.ndomcon_solver.NormalDominanceConstraintSolver( \
                fragments, cons );
-    solver.solve();
+    if not solver.solve():
+      return None;
+    
+    print "CHART IDX"+str(solver._chart_keys);
+    print "CHART"+str(solver._chart);
+    
+    
+    found = False;
+    idxs = [];
+    for i in range( 0, len(solver._chart_keys) ):
+      if len( solver._chart_keys[ i ] ) == len( fragments.keys() ):
+        found = True;
+        for root in solver._chart_keys[ i ]:
+          if not root in fragments.keys():
+            found = False;
+        if found:
+          idxs.append( i );
+    
+    print idxs;
+    for idx in idxs:
+      print "I"+str( solver._chart[ idx ] );
     
     return [ self.get_lr_scoping() ];
     
--- src/pyrmrs/tools/ndomcon_solver.py
+++ src/pyrmrs/tools/ndomcon_solver.py
@@ -1,3 +1,4 @@
+import pyrmrs.globals;
 import copy;
 
 
@@ -7,10 +8,7 @@ class NormalDominanceConstraintSolver:
   _fragments = {};
   
   _dom_cons = {};
-  _eq_cons = {};
-  
   _dom_cons_inv = {};
-  _eq_cons_inv = {};
   
   _reachability_cache = None;
   
@@ -19,11 +17,13 @@ class NormalDominanceConstraintSolver:
 
 
 
-  def __init__( self, fragments, dom_cons={}, eq_cons={} ):
+  def __init__( self, fragments, dom_cons={} ):
 
     self._fragments = fragments;
     self._dom_cons = dom_cons;
-    self._eq_cons = eq_cons;
+    
+    pyrmrs.globals.logDebug( self, str( self._fragments ) );
+    pyrmrs.globals.logDebug( self, str( self._dom_cons ) );
     
     self._chart = {};
     self._chart_keys = [];
@@ -35,49 +35,42 @@ class NormalDominanceConstraintSolver:
           self._dom_cons_inv[ root ] = [];
         self._dom_cons_inv[ root ].append( hole );
 
-    self._eq_cons_inv = {};
-    for hole in eq_cons:
-      for root in eq_cons[ hole ]:
-        if not self._eq_cons_inv.has_key( root ):
-          self._eq_cons_inv[ root ] = [];
-        self._eq_cons_inv[ root ].append( hole );
 
 
-
-  def _reachable( self, v1, v2, roots_=None, eq_cons=[] ):
+  def _reachable( self, v1, v2, roots_=None ):
+    
+    if v1 == (True, 4) and v2 == (False, 6) and roots_ == [(True, 1), (True, 4)]:
+      pass;
     
     roots = None;
     if roots_ is None:
-      roots = self._fragments.keys();
+      roots = copy.copy( self._fragments.keys() );
     else:
       roots = copy.copy( roots_ );
       
     i = 0;
     reachable = [];
     if not self._reachability_cache is None:
-      ( roots_, eq_cons_, reachable_, i_ ) = self._reachability_cache;
-      if roots == roots_ and eq_cons == eq_cons_:
+      ( roots_, reachable_, i_ ) = self._reachability_cache;
+      if roots == roots_:
         reachable = reachable_;
         i = i_;
+        pass;
     
     if reachable == []:
       for root in roots:
         for hole in self._fragments[ root ]:
-          if ( not ( (True,root), (False,hole) ) in reachable ) and \
-             ( not ( (False,hole), (True,root) ) in reachable ):
-            reachable.append( ( (True,root), (False,hole) ) );
-      for hole in self._dom_cons:
-        for root in self._dom_cons[ hole ]:
-          if ( not ( (True,root), (False,hole) ) in reachable ) and \
-             ( not ( (False,hole), (True,root) ) in reachable ):
-            reachable.append( ( (True,root), (False,hole) ) );
-      for hole in eq_cons:
-        for root in eq_cons[ hole ]:
-          if ( not ( (True,root), (False,hole) ) in reachable ) and \
-             ( not ( (False,hole), (True,root) ) in reachable ):
-            reachable.append( ( (True,root), (False,hole) ) );
+          if ( not ( root, hole ) in reachable ) and \
+             ( not ( hole, root ) in reachable ):
+            reachable.append( (root,hole) );
+      for higher in self._dom_cons:
+        for lower in self._dom_cons[ higher ]:
+          if not ( higher[0] and not higher in roots ):
+            if ( not ( higher, lower ) in reachable ) and \
+               ( not ( lower, higher ) in reachable ):
+              reachable.append( (higher,lower) );
 
-    self._reachability_cache = ( roots, eq_cons, reachable, i );
+    self._reachability_cache = ( roots, reachable, i );
     if (v1,v2) in reachable or (v2,v1) in reachable:
       return True;
 
@@ -122,7 +115,7 @@ class NormalDominanceConstraintSolver:
               if ( a1 == v1 and b1 == v2 ) or ( a1 == v2 and b1 == v1 ):
                 break;
     
-    self._reachability_cache = ( roots, eq_cons, reachable, i );
+    self._reachability_cache = ( roots, reachable, i );
     if (v1,v2) in reachable or (v2,v1) in reachable:
       return True;
     else:
@@ -130,19 +123,21 @@ class NormalDominanceConstraintSolver:
 
 
 
-  # GRAP-SOLVER-CHART(G')
+  # GRAPH-SOLVER-CHART(G')
   def solve( self, roots_=None ):
 
     roots = None;
     if roots_ is None:
-      roots = self._fragments.keys();
+      roots = copy.copy( self._fragments.keys() );
     else:
       roots = copy.copy( roots_ );
-      
+
     # if there is an entry for G' in the chart
     if roots in self._chart_keys:
       # then return true
       return True;
+
+    pyrmrs.globals.logDebug( self, "R"+str(roots) );
     
     # if G' contains only one fragment
     if len(roots) == 1:
@@ -150,51 +145,77 @@ class NormalDominanceConstraintSolver:
       return True;
     
     # free <- FREE-FRAGMENTS(G')
-    
     free_roots = [];
-    
     for k in range( 0, len(roots) ):
-      
       root = roots[ k ];
-      
-      if self._dom_cons_inv.has_key( root ) or self._eq_cons_inv.has_key( root ):
+      if self._dom_cons_inv.has_key( root ):
         continue;
-      
       del roots[ k ];
-      
       fragment = self._fragments[ root ];
       free = True;
       for i in range( 0, len(fragment) ):
         for j in range( 0, i ):
-          if self._reachable( ( False, fragment[i] ), ( False, fragment[j] ), roots, [] ):
+          if self._reachable( fragment[i], fragment[j], roots ):
             free = False;
       if free:
         free_roots.append( root );
-        
       roots.insert( k, root );
-
     # assert free == FREE-FRAGMENTS(G')
-    
-    print free_roots; # for now
-    return True; # for now
 
-  
+    pyrmrs.globals.logDebug( self, "F"+str(free_roots) );
 
     # if free = emptyset
     if len(free_roots) == 0:
       # then return false
       return False;
-    
-    # for each f in free
-    for root in free_roots:
-      subordinate_roots = {};
-      for hole in self._fragments[ root ]:
-        subordinate_roots[ hole ];
-        
 
+    # for each F in free
+    for free_root in free_roots:
+
+      pyrmrs.globals.logDebug( self, "R"+str(roots) );
+      pyrmrs.globals.logDebug( self, "F"+str(free_roots) );
+      pyrmrs.globals.logDebug( self, "-->"+str(free_root) );
       
+      # split <- SPLIT(G',F)
+      roots.remove( free_root );
+      split = {};
+      assigned = [];
+      empty_hole = [];
+      for hole in self._fragments[ free_root ]:
+        split[ hole ] = [];
+        for root in self._fragments:
+          if self._reachable( root, hole, roots ):
+            split[ hole ].append( root );
+            assigned.append( root );
+      for hole in split:
+        if split[ hole ] == []:
+          empty_hole.append( hole );
+
+      assert len( empty_hole ) <= 1;
+
+      if len( empty_hole ) == 1:
+        empty_hole = empty_hole[ 0 ];
+        split[ empty_hole ] = [];
+        for root in roots:
+          if not root in assigned:
+            split[ empty_hole ].append( root );
+        if ( True, 1 ) in split[ empty_hole ]:
+          pass;
+      roots.append( free_root );
+      # assert split == SPLIT(G',F)
+
+      pyrmrs.globals.logDebug( self, "S"+str(split) );
       
-    
-    
-    
-    
+      # for each S in WCCS(G'-F)
+      for hole in split:
+        # if GRAPH-SOLVER-CHART(S) == false
+        if not self.solve( split[ hole ] ):
+          # return false
+          return False;
+      
+      # add (G',split) to the chart
+      self._chart_keys.append( roots );
+      self._chart[ len( self._chart_keys )-1 ] = split;
+      
+    # return true
+    return True;
