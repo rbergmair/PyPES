c350cc2f90fb963b90cfa5614cf3b9048c9cf01d
Richard Bergmair <r.bergmair.gh@rb.uref.email>
2008-03-19 19:56:08 +0000

    git-svn-id: file:///dta/svn/old/PyPES/trunk@79 91a45504-6d28-49e1-87dd-05d800df9125

--- src/sanitize_items.py
+++ src/sanitize_items.py
@@ -1,32 +1,50 @@
 import codecs;
 
 import os;
+
 import string;
+import re;
 
 import pyrmrs.globals;
 import pyrmrs.config;
 
 pyrmrs.globals.initMain();
 
+INDIR = "testdta/items-in/rte";
+OUTDIR = "testdta/items-pre/rte";
+
+
+
 EOS_PUN = [ ".", "!", "?", "\"" ];
 PUN = EOS_PUN + [ "," ];
 
-INDIR = "testdta/items-in/rte";
-OUTDIR = "testdta/items/rte";
+DATEMARKERs = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov",
+    "Dec",
+    "January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December",
+    "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun",
+    "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+  ];
+
+HEADLINEMARKERs = [
+    "Reuters", "Dow Jones", "Bloomberg", "AP", "CNN", "BBC"
+  ];
 
 
 
 for fname in os.listdir( INDIR ):
-#for fname in [ "rte1-cd.items" ]:
+#for fname in [ "testdta/all-examples.txt" ]:
 
   
   infile = codecs.open( INDIR+"/"+fname, "r", encoding="utf-8" );
+  #infile = codecs.open( fname, "r", encoding="utf-8" );
   
   items_by_len = {};
   k = 0;
   
   for line in infile:
-
+    
     #k += 1;
     #if k >= 300:
     #  break;
@@ -47,7 +65,7 @@ for fname in os.listdir( INDIR ):
     currenttoken = "";
     previoustoken = "";
     
-    for ch in line+" ":
+    for ch in line+" x":
       x = string.whitespace.find( ch ) == -1;
       if x:
         if block:
@@ -66,7 +84,7 @@ for fname in os.listdir( INDIR ):
         if not block:
           tokens += 1;
           block = True;
-    outline = outline[ :len(outline)-1 ];      
+    outline = outline[ :len(outline)-2 ];      
           
     if line != outline:
       changes.append( 1 );
@@ -83,11 +101,81 @@ for fname in os.listdir( INDIR ):
     if line != outline:
       changes.append( 3 );
       line = outline;
+
+    outline = outline.replace( "( ", "(" );
+    outline = outline.replace( "[ ", "[" );
+    outline = outline.replace( " )", ")" );
+    outline = outline.replace( " ]", "]" );
+
+    if line != outline:
+      changes.append( 4 );
+      line = outline;
+      
+    outline = outline.replace( " -- ", " - " );
+    outline = outline.replace( "-- ", " - " );
+    outline = outline.replace( " --", " - " );
+    outline = outline.replace( "--", " - " );
+    
+    regex = re.compile( "[^\s]- " );
+    while True:
+      r = regex.search( outline );
+      if not r is None:
+        prefix = outline[ :r.start()+1 ];
+        suffix = outline[ r.end(): ];
+        outline = prefix + " - " + suffix;
+      else:
+        break;
+
+    regex = re.compile( " -[^\s0-9]" );
+    while True:
+      r = regex.search( outline );
+      if not r is None:
+        prefix = outline[ :r.start() ];
+        suffix = outline[ r.end()-1: ];
+        outline = prefix + " - " + suffix;
+      else:
+        break;
+    
+    if line != outline:
+      changes.append( 5 );
+      line = outline;
+      
+    r=outline.find( " - " );
+    if r != -1:
+      prefix = outline[ :r ];
+      nextchar = outline[ r+3 ];
+      isupcase = string.ascii_uppercase.find( nextchar ) != -1;
+      
+      isheadline = False;
+      
+      if isupcase:
+        cnt = prefix.count( " " );
+        if cnt == 0:
+          isheadline = True;
+        elif cnt < 10:
+          for marker in DATEMARKERs + HEADLINEMARKERs:
+            if prefix.find( marker ) != -1:
+              isheadline = True;
+              break;
+          regex = re.compile( "[0-9]+\s*[/\.\-]\s*[0-9]+\s*[/\.\-]\s*[0-9]+" );
+          if not regex.search( prefix ) is None:
+            isheadline = True;
+          
+      if isheadline:
+        outline = prefix + " ||| " + outline[ r+3: ];
+
+    if line != outline:
+      changes.append( 6 );
+      line = outline;
+      
+      
       
     if origline != outline:
       print "applied sanitizers %s" % str( changes );
       print "   in  \"%s\"" % origline;  
       print "  out  \"%s\"" % outline;  
+    else:
+      print "nosan  \"%s\"" % outline;  
       
     
     if not items_by_len.has_key( tokens ):
@@ -95,6 +183,8 @@ for fname in os.listdir( INDIR ):
     items_by_len[ tokens ].append( outline );
     
   infile.close();
+  
+  # break;
 
 
   maxlen = 0;
