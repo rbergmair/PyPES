bc21ba5493645cfe3bf49f7d6e015e1db34b8650
Richard Bergmair <r.bergmair.gh@rb.uref.email>
2008-04-28 16:07:44 +0000

    git-svn-id: file:///dta/svn/old/PyPES/trunk@91 91a45504-6d28-49e1-87dd-05d800df9125

--- src/test_textcontent_filter.py
+++ src/test_textcontent_filter.py
@@ -2,6 +2,31 @@ import pyrmrs.globals;
 import pyrmrs.xmltools.textcontent_filter;
 
 import sys;
+import codecs;
+
+
+
+pyrmrs.globals.initMain();
+
+
+
+class MyTxtCntFilter( pyrmrs.xmltools.textcontent_filter.TextContentFilter ):
+
+  def __init__( self ):
+    
+    pyrmrs.xmltools.textcontent_filter.TextContentFilter.__init__( self );
+    self.registerTextFilter( "p", self.filter );
+  
+  def filter( self, text ):
+    
+    return text.replace( "linguistic", "xlinguistic" );
+
+f = open( "testdta/testdta.xhtml.xml" );
+g = open( "testdta/testdta-out.xhtml.xml", "w" );
+flt = MyTxtCntFilter();
+flt.processAll( f, g );
+g.close();
+f.close();
 
 
 
@@ -9,19 +34,14 @@ def filter( text ):
   
   return text.replace( "linguistic", "xlinguistic" );
 
-
-
-pyrmrs.globals.initMain();
-
 f = open( "testdta/testdta.xhtml.xml" );
-g = open( "testdta/testdta-out.xhtml.xml", "w" );
-
-
-txtcntflt = pyrmrs.xmltools.textcontent_filter.TextContentFilter();
-txtcntflt.registerTextFilter( "p", filter );
-txtcntflt.processAll( f, g );
-
+g = codecs.open( "testdta/testdta-out-2.xhtml.xml", "w", encoding="utf-8" );
+flt = pyrmrs.xmltools.textcontent_filter.TextContentFilter();
+flt.registerTextFilter( "p", filter );
+flt.processAll( f, g );
 g.close();
 f.close();
+
+
 
 pyrmrs.globals.destructMain();
