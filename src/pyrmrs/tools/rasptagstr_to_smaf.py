30774435f0d44af7405b07453af6fcc36579fd5b
Richard Bergmair <r.bergmair.gh@rb.uref.email>
2008-05-14 23:37:07 +0000

    git-svn-id: file:///dta/svn/old/PyPES/trunk@96 91a45504-6d28-49e1-87dd-05d800df9125

--- src/pyrmrs/tools/rasptagstr_to_smaf.py
+++ src/pyrmrs/tools/rasptagstr_to_smaf.py
@@ -6,16 +6,18 @@ import pyrmrs.smafpkg.pos_edge;
 import re;
 
 MAP = {
-  "&FO" : "UNC",
-  "$" : "POS"
+#  "&FO" : "UNC",
+#  "$" : "POS"
 };
 
-def raspstr_to_smaf( surface, raspstr ):
+wspan = re.compile( "(<w s=')([0-9]+)(' e=')([0-9]+)('>)(.*)(</w>)" );
+number = re.compile( "([0-9]+)(\.[0-9]+(e[+-]?[0-9]+)?)?" );
+
+
+def rasptagstr_to_smaf( surface, raspstr ):
   
   smaf = pyrmrs.smafpkg.smaf.SMAF();
   lattice = pyrmrs.smafpkg.lattice.Lattice();
-  
-  raspstr = raspstr[ : raspstr.find( "\n" ) ];
   
   # print surface;
   
@@ -35,61 +37,90 @@ def raspstr_to_smaf( surface, raspstr ):
     i += 1;
 
   i = 1;
+  peid = 1;
   
-  for token in raspstr.split( "|" ):
+  for token in raspstr.split( "\n" ):
     
-    if not ( token.find( "<w s='" ) == 0 and token.endswith( "</w>" ) ):
-      continue;
-    
-    r = re.compile( "(<w s=')([0-9]+)(' e=')([0-9]+)('>.*_)(.*)(</w>)" );
-    m = r.match( token );
+    m = wspan.search( token );
     if m is None:
       assert False;
-    else:
+    
+    grps = m.groups();
       
-      s = int( m.groups()[ 1 ] );
-      e = int( m.groups()[ 3 ] );
-      pos = m.groups()[ 5 ];
+#     print token;
+#     print m.groups();
+#     print len( m.groups() );
+    
+    s = int( grps[1] );
+    e = int( grps[3] );
+    tx2 = grps[ 5 ];
+    
+    pos_txt = token[ m.end( len(grps) ): ];
+    pos_txt = pos_txt.strip();
+    
+#    print pos_txt;
 
-      if min is None:
-        min = s;
-      max = e;
+    if min is None:
+      min = s;
+    max = e;
 
-      s -= min;
-      e -= min;
+    s -= min;
+    e -= min;
+    
+    s = utf8offs[s];
+    e = utf8offs[e];
+
+    e += 1;
       
-      s = utf8offs[s];
-      e = utf8offs[e];
+    txt = surface[ s : e ];
+    
+    assert txt == tx2;
+    
+    edge = pyrmrs.smafpkg.token_edge.TokenEdge();
+    
+    edge.id = "t%d" % i;
+    
+    edge.source = "v%d" % (i-1);
+    edge.target = "v%d" % i;
+    edge.cfrom = s;
+    edge.cto = e;
+    
+    edge.text = txt;
 
-      e += 1;
-        
-      txt = surface[ s : e ];
+    lattice.register( edge );
+    
+    for postag in pos_txt.split(" "):
       
-      edge = pyrmrs.smafpkg.token_edge.TokenEdge();
-      edge.id = "t%d" % i;
-      edge.source = "v%d" % (i-1);
-      edge.target = "v%d" % i;
-      edge.cfrom = s;
-      edge.cto = e;
-      edge.text = txt;
-
-      lattice.register( edge );
+      postagspl = postag.split( ":" );
+#      print postagspl;
+      
+      tag = postagspl[0];
+      probtxt = postagspl[1];
+      
+      m = number.search( probtxt );
+      prob = probtxt[ m.start(0) : m.end( len( m.groups() ) ) ];
       
       posedge = pyrmrs.smafpkg.pos_edge.PosEdge();
-      posedge.id = "p%d" % i;
+      
+      posedge.id = "p%d" % peid;
+      peid += 1;
+      
       posedge.source = edge.source;
       posedge.target = edge.target;
       posedge.cfrom = edge.cfrom;
       posedge.cto = edge.cto;
       posedge.deps = edge.id;
-      if MAP.has_key( pos ):
-        posedge.tag = MAP[ pos ];
+      
+      posedge.weight = prob;
+      
+      if MAP.has_key( tag ):
+        posedge.tag = MAP[ tag ];
       else:
-        posedge.tag = pos;
-
+        posedge.tag = tag;
+  
       lattice.register( posedge );
 
-      i += 1;
+    i += 1;
   
   lattice.init = "v0";
   lattice.final = "v%d" % (i-1);
