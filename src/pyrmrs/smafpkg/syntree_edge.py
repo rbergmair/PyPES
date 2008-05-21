9394b246d96d0d9922973140fbce26b77c28a6ef
Richard Bergmair <r.bergmair.gh@rb.uref.email>
2008-05-21 11:31:30 +0000

    git-svn-id: file:///dta/svn/old/PyPES/trunk@104 91a45504-6d28-49e1-87dd-05d800df9125

---
+++ src/pyrmrs/smafpkg/syntree_edge.py
@@ -0,0 +1,37 @@
+import edge;
+import slot;
+
+class PosEdge( edge.Edge ):
+  
+  type = "pos";
+  tag = None;
+  weight = None;
+
+  def __init__( self ):
+    
+    edge.Edge.__init__( self );
+    self.tag = None;
+    self.weight = None;
+    self.type = "pos";
+    
+  def register( self, obj ):
+    
+    edge.Edge.register( self, obj );
+    if isinstance( obj, slot.Slot ):
+      if obj.name == "tag":
+        self.tag = obj.text;
+      if obj.name == "weight":
+        self.weight = obj.text;
+      
+  def xml_tmplt( self, base ):
+    
+    base = edge.Edge.xml_tmplt ( self, base );
+    elements = "";
+    if not self.tag is None:
+      elements += "\n  "+slot.Slot( "tag", self.tag ).str_xml();
+      elements += "\n  "+slot.Slot( "weight", self.weight ).str_xml();
+    elements += "\n";
+    elements = elements.replace( "%", "%%" );
+    
+    base = base.replace( "%%", "%%%%" );
+    return base % ( "%s", elements+"%s" );
