45f166ada7aef5f35d2754d5dbc8921429a21e80
Richard Bergmair <r.bergmair.gh@rb.uref.email>
2008-12-11 19:47:48 +0000

    git-svn-id: file:///dta/svn/old/PyPES/trunk@200 91a45504-6d28-49e1-87dd-05d800df9125

---
+++ PyRBUtils/src/pyrbutilstest/xml/reader.py
@@ -0,0 +1,15 @@
+# -*-  coding: ascii -*-
+
+class XMLReaderTest:
+
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                             #
+# (c) Copyright 2009 by Richard Bergmair.                                     #
+#                                                                             #
+#   See LICENSE.txt for terms and conditions                                  #
+#   on use, reproduction, and distribution.                                   #
+#                                                                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
