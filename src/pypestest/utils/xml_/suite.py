39b029a844552abfd744505aecaed0c91d29746c
Richard Bergmair <r.bergmair.gh@rb.uref.email>
2009-01-08 09:09:34 +0000

    git-svn-id: file:///dta/svn/old/PyPES/trunk@227 91a45504-6d28-49e1-87dd-05d800df9125

---
+++ src/pypestest/utils/xml_/suite.py
@@ -0,0 +1,40 @@
+# -*-  coding: ascii -*-
+
+__package__ = "pypestest.utils";
+
+import sys;
+import unittest;
+
+import pypestest.utils.xml_.text_content_filter;
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+def suite():
+
+  suite = unittest.TestSuite();
+
+  suite.addTests( pypestest.utils.xml_.text_content_filter.suite() );
+
+  return suite;
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+def main( argv=None ):
+
+  unittest.TextTestRunner( verbosity=2 ).run( suite() );
+
+if __name__ == '__main__':
+  sys.exit( main( sys.argv ) );
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                             #
+#        PyPES: the python platform for experimentation with semantics        #
+#                                                                             #
+#                  (c) Copyright 2009 by Richard Bergmair                     #
+#       -----------------------------------------------------------------     #
+#       See LICENSE.txt for terms and conditions on use and reproduction.     #
+#                                                                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
