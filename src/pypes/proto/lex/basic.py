d9c3cf7f3da7cbf7d08df75b70bdc55160d437ad
Richard Bergmair <r.bergmair.gh@rb.uref.email>
2009-02-05 17:26:54 +0000

    git-svn-id: file:///dta/svn/old/PyPES/trunk@277 91a45504-6d28-49e1-87dd-05d800df9125

---
+++ src/pypes/proto/lex/basic.py
@@ -0,0 +1,190 @@
+# -*-  coding: ascii -*-  # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+__package__ = "pypes.proto.lex";
+__all__ = [ "Word", "Operator" ];
+
+from pypes.utils.mc import kls;
+from pypes.proto.protobase import ProtoBase;
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+class Word( ProtoBase, metaclass=kls ):
+
+
+  _superordinate_ = "sig";
+  _key_ = "wid";
+
+
+  def _init_init_( self ):
+    
+    self.wid = None;
+    self.lemma = None;
+    self.pos = None;
+    self.sense = None;
+    self.feats = None;
+
+  
+  def __init__( self, sig, wid=None, lemma=None, pos=None,
+                      sense=None, feats=None ):
+    
+    if wid is not None:
+      self.wid = wid;
+    
+    if lemma is not None:
+      self.lemma = lemma;
+      assert self.lemma is None or isinstance( self.lemma, list );
+    
+    if pos is not None:
+      self.pos = pos;
+      assert self.pos is None or isinstance( self.pos, str );
+    
+    if sense is not None:
+      self.sense = sense;
+      assert self.sense is None or isinstance( self.sense, str );
+    
+    if feats is not None:
+      self.feats = {};
+      for (key,val) in feats.items():
+        if key == "pos":
+          self.pos = val;
+        elif key == "sense":
+          self.sense = val;
+        else:
+          self.feats[ key ] = val;
+        
+  
+  def __le__( self, obj ):
+    
+    if not isinstance( obj, Word ):
+      return False;
+    
+    if self.wid is not None:
+      if self.wid != obj.wid:
+        return False;
+      
+    if self.lemma is not None:
+      if self.lemma != obj.lemma:
+        return False;
+      
+    if self.pos is not None:
+      if self.pos != obj.pos:
+        return False;
+      
+    if self.sense is not None:
+      if self.sense != obj.sense:
+        return False;
+    
+    if self.feats is not None:
+      if obj.feats is None:
+        return False;
+      for feat in self.feats:
+        if not feat in obj.feats:
+          return False;
+        if self.feats[ feat ] != obj.feats[ feat ]:
+          return False;
+      
+    return True;
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+class Operator( ProtoBase, metaclass=kls ):
+
+
+  _superordinate_ = None;
+  _key_ = None;
+
+  
+  OP_Q_UNIV = "ALL";
+  OP_Q_EXIST = "SOME";
+  OP_Q_DESCR = "THE";
+  
+  OP_Qs = {
+      OP_Q_UNIV: OP_Q_UNIV,
+      OP_Q_EXIST: OP_Q_EXIST,
+      OP_Q_DESCR: OP_Q_DESCR
+    };
+
+  OP_C_STRCON = "&&";
+  OP_C_WEACON = "/\\";
+  OP_C_STRDIS = "||";
+  OP_C_WEADIS = "\\/";
+  OP_C_IMPL = "->";
+  
+  OP_Cs = {
+      OP_C_STRCON: OP_C_STRCON,
+      OP_C_WEACON: OP_C_WEACON,
+      OP_C_STRDIS: OP_C_STRDIS,
+      OP_C_WEADIS: OP_C_WEADIS,
+      OP_C_IMPL: OP_C_IMPL
+    };
+  
+  OP_M_NECESSITY = "NECESSARILY";
+  OP_M_POSSIBILITY = "POSSIBLY";
+  
+  OP_Ms = {
+      OP_M_NECESSITY: OP_M_NECESSITY,
+      OP_M_POSSIBILITY: OP_M_POSSIBILITY
+    };
+
+  OP_P_EQUALITY = "EQUALS";
+  
+  OP_Ps = {
+      OP_P_EQUALITY: OP_P_EQUALITY
+    };
+
+
+  def _init_init_( self ):
+    
+    self.otype = None;
+    self.feats = None;
+    self._ops = {};
+    self._ops.update( self.OP_Qs );
+    self._ops.update( self.OP_Cs );
+    self._ops.update( self.OP_Ms );
+    self._ops.update( self.OP_Ps );
+
+  
+  def __init__( self, sig, otype=None, feats=None ):
+    
+    if otype is not None:
+      assert otype in self._ops;
+      self.otype = otype;
+    
+    if feats is not None:
+      self.feats = feats;
+
+  
+  def __le__( self, obj ):
+    
+    if not isinstance( obj, self.__class__ ):
+      return False;
+    
+    if self.otype is not None:
+      if self.otype != obj.otype:
+        return False;
+    return True;
+    if self.feats is not None:
+      if obj.feats is None:
+        return False;
+      for feat in self.feats:
+        if not feat in obj.feats:
+          return False;
+        if self.feats[ feat ] != obj.feats[ feat ]:
+          return False;
+    return True;
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                             #
+#        PyPES: the python platform for experimentation with semantics        #
+#                                                                             #
+#                  (c) Copyright 2009 by Richard Bergmair                     #
+#       -----------------------------------------------------------------     #
+#       See LICENSE.txt for terms and conditions on use and reproduction.     #
+#                                                                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
