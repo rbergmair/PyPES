6d9f1710f725055d2f322efefeb351504e6f0220
Richard Bergmair <r.bergmair.gh@rb.uref.email>
2009-02-24 17:01:54 +0000

    git-svn-id: file:///dta/svn/old/PyPES/trunk@335 91a45504-6d28-49e1-87dd-05d800df9125

---
+++ src/pypes/rewrite/dsnf.py
@@ -0,0 +1,89 @@
+# -*-  coding: ascii -*-  # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+__package__ = "pypes.rewrite";
+__all__ = [ "DSNFRewriter" ];
+
+from pypes.utils.mc import subject;
+
+from pypes.proto import *;
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+class DSNFRewriter( metaclass=subject ):
+
+
+  def _enter_( self ):
+
+    self._sig = ProtoSig();
+    self._pf = self._obj_( sig=self._sig );
+
+
+  def _collect_subforms_by_var( self, pf );
+
+    for subf in pf.subforms.values():
+
+      if isinstance( subf, Predication ):
+
+        for arg in subf.args.values():
+          if isinstance( arg, Variable ):
+            if not arg in self._subforms_by_var:
+              self._subforms_by_var[ arg ] = set();
+            self._subforms_by_var[ arg ].add( subf );
+
+      elif isinstance( subf, Modification ):
+
+        for arg in subf.args.values():
+          if isinstance( arg, Variable ):
+            if not arg in self._subforms_by_var:
+              self._subforms_by_var[ arg ] = set();
+            self._subforms_by_var[ arg ].add( subf );
+
+        self._collect_subforms_by_var( subf.scope );
+
+      elif isinstance( subf, Quantification ):
+
+        self._collect_subforms_by_var( subf.rstr );
+        self._collect_subforms_by_var( subf.body );
+
+      elif isinstance( subf, Connection ):
+
+        self._collect_subforms_by_var( subf.lscope );
+        self._collect_subforms_by_var( subf.rscope );
+
+
+  def rewrite( self ):
+
+    self._subforms_by_var = {};
+    self._collect_subforms_by_var( self._pf );
+    self._subform_quant = [];
+
+    for var in self._sig._sos_[ Variable ].values():
+
+      quant = [];
+
+      for subf in self._subforms_by_var[ var ]:
+        if isinstance( subf, Quantification ):
+          subf.quantifier.referent.
+
+      assert len( quant ) == 1;
+
+      for subf in self._subforms_by_var
+
+      
+
+    
+  
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                             #
+#        PyPES: the python platform for experimentation with semantics        #
+#                                                                             #
+#                  (c) Copyright 2009 by Richard Bergmair                     #
+#       -----------------------------------------------------------------     #
+#       See LICENSE.txt for terms and conditions on use and reproduction.     #
+#                                                                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
