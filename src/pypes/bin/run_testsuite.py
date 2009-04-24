b12db9b6d1a16ec3876ee4a22d3222445934dcb9
Richard Bergmair <r.bergmair.gh@rb.uref.email>
2009-04-24 10:32:26 +0000

    git-svn-id: file:///dta/svn/old/PyPES/trunk@441 91a45504-6d28-49e1-87dd-05d800df9125

---
+++ src/pypes/bin/run_testsuite.py
@@ -0,0 +1,29 @@
+# -*-  coding: ascii -*-  # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+import sys;
+
+from pypes.infer.runner import run_testsuite;
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+def main( argv=None ):
+  
+  run_testsuite( "dta/infer/fracas/fracas-1-1", "dta/items/fracas" );
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+if __name__ == '__main__':
+  sys.exit( main( sys.argv ) );
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                             #
+#        PyPES: the python platform for experimentation with semantics        #
+#                                                                             #
+#                  (c) Copyright 2009 by Richard Bergmair                     #
+#       -----------------------------------------------------------------     #
+#       See LICENSE.txt for terms and conditions on use and reproduction.     #
+#                                                                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
