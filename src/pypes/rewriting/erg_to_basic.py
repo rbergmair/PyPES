aee7aeb36acc421bc6bafe6c3c70c0fd1c78594b
Richard Bergmair <r.bergmair.gh@rb.uref.email>
2009-05-26 16:08:22 +0000

    git-svn-id: file:///dta/svn/old/PyPES/trunk@504 91a45504-6d28-49e1-87dd-05d800df9125

--- src/pypes/rewriting/erg_to_basic.py
+++ src/pypes/rewriting/erg_to_basic.py
@@ -234,9 +234,14 @@ class ERGtoBasic( ProtoProcessor, metaclass=subject ):
     };
   
   NONCTRL_WRD_Ps = [
+                    
       ( (['be'], 'v', 'there'), basic.Operator.OP_P_TAUTOLOGY ),
+      
       ( (['be'], 'v', 'id'), basic.Operator.OP_P_EQUALITY ),
-      ( (['people'], 'n', 'of'), erg.Operator.PERSON ),
+      ( (['lot'], 'n', 'of'), basic.Operator.OP_P_EQUALITY ),
+      
+      ( (['people'], 'n', 'of'), erg.Operator.PERSON )
+      
     ];
   
   
