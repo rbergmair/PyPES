b880fd9945b37cb7e1258f3f537759a6ef3175a7
Richard Bergmair <r.bergmair.gh@rb.uref.email>
2009-08-26 08:23:41 +0000

    git-svn-id: file:///dta/svn/old/PyPES/trunk@559 91a45504-6d28-49e1-87dd-05d800df9125

---
+++ src/pypes/bin.py
@@ -0,0 +1,36 @@
+# -*-  coding: ascii -*-  # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+__package__ = "pypes";
+__all__ = [
+    "sanitize_rte", "preprocess_rte", "preprocess_rte_results",
+    "extract_ergsem_smi", "extract_logpats", "read_treebank", "run_testsuite",
+    "reconsider_decisions", "compare_decisions", "score_decisions"
+  ];
+
+
+from pypes.infer._preprocessing.rte_sanitizer import sanitize_rte;
+from pypes.infer._preprocessing.rte_preprocessor import  preprocess_rte;
+from pypes.infer._preprocessing.rte_results import  preprocess_rte_results;
+
+from pypes.codecs_.mrs._smi.ergsem_smi_extractor import extract_ergsem_smi;
+
+from pypes.rewriting._logpat_extractor import extract_logpats;
+
+from pypes.infer._preprocessing.read_treebank import read_treebank;
+
+from pypes.infer.testsuite_runner import run_testsuite;
+
+from pypes.infer._evaluation.reconsider_decisions import reconsider_decisions;
+from pypes.infer._evaluation.compare_decisions import compare_decisions;
+from pypes.infer._evaluation.score_decisions import score_decisions;
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                             #
+#        PyPES: the python platform for experimentation with semantics        #
+#                                                                             #
+#                  (c) Copyright 2009 by Richard Bergmair                     #
+#       -----------------------------------------------------------------     #
+#       See LICENSE.txt for terms and conditions on use and reproduction.     #
+#                                                                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
